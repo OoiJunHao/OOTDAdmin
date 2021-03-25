1d464d586ae348bae995bdb54d91b3b2d1321a50
Ong Bik Jeun <e0426272@u.nus.edu>
2021-03-25 20:24:12 +0800

    fix some header issue

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,7 +10,6 @@ import { MatButtonModule } from '@angular/material/button';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './footer/footer.component';
-import { HeaderComponent } from './header/header.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MainPageComponent } from './main-page/main-page.component';
 import { HeaderNavComponent } from './header-nav/header-nav.component';
@@ -20,7 +19,6 @@ import { LayoutModule } from '@angular/cdk/layout';
   declarations: [
     AppComponent,
     FooterComponent,
-    HeaderComponent,
     MainPageComponent,
     HeaderNavComponent
   ],
