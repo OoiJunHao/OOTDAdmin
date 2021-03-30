0b150ac5f6d9f20b087e6d03f88c119a8e5ccce3
Ong Bik Jeun <e0426272@u.nus.edu>
2021-03-30 18:22:24 +0800

    review

---
+++ src/app/services/review-management.service.ts
@@ -0,0 +1,40 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { Review } from '../models/review';
+import { SessionService } from './session.service';
+
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ReviewManagementService {
+  baseUrl: string = '/api/reviewManagement'
+
+  constructor(private httpClient: HttpClient, private sessionService: SessionService) { }
+
+  getReviews(): Observable<Review[]> {
+    return this.httpClient.get<Review[]>(this.baseUrl + "/retrieveAllReviews?username=" + this.sessionService.getUsername() + "&password=" + this.sessionService.getPassword()).pipe(
+      catchError(this.handleError)
+    );
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    let errorMessage: string = "";
+
+    if (error.error instanceof ErrorEvent) {
+      errorMessage = "An unknown error has occurred: " + error.error;
+    }
+    else {
+      errorMessage = "A HTTP error has occurred: " + `HTTP ${error.status}: ${error.error}`;
+    }
+
+    console.error(errorMessage);
+
+    return throwError(errorMessage);
+  }
+}
--- src/app/view-all-reviews/view-all-reviews.component.ts
+++ src/app/view-all-reviews/view-all-reviews.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Review } from '../models/review';
 
 @Component({
   selector: 'app-view-all-reviews',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ViewAllReviewsComponent implements OnInit {
 
-  constructor() { }
+  reviews: Review[];
+
+  constructor() {
+    this.reviews = new Array();
+  }
 
   ngOnInit(): void {
+
   }
 
 }
