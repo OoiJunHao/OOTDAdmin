e20b36104f8ba04cf7e704d40ae3362b0ebb8f9a
Ong Bik Jeun <e0426272@u.nus.edu>
2021-03-30 10:58:21 +0800

    minor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,22 +9,17 @@ import { TableModule } from 'primeng/table';
 import { DialogModule } from 'primeng/dialog';
 import { DropdownModule } from 'primeng/dropdown';
 import { CheckboxModule } from 'primeng/checkbox';
-
-import {SplitButtonModule} from 'primeng/splitbutton';
-import {ConfirmDialogModule} from 'primeng/confirmdialog';
+import { SplitButtonModule } from 'primeng/splitbutton';
+import { ConfirmDialogModule } from 'primeng/confirmdialog';
 import { ConfirmationService } from 'primeng/api';
 import { CardModule } from 'primeng/card';
 import { ButtonModule } from 'primeng/button';
 import { PanelModule } from 'primeng/panel';
-import { SplitButtonModule } from 'primeng/splitbutton';
 import { MessageModule } from 'primeng/message';
 import { MessagesModule } from 'primeng/messages';
-import { ConfirmDialogModule } from 'primeng/confirmdialog';
 import { ToolbarModule } from 'primeng/toolbar';
 import { DividerModule } from 'primeng/divider';
-import {MessagesModule} from 'primeng/messages';
-import {MessageModule} from 'primeng/message';
-import {ToastModule} from 'primeng/toast';
+import { ToastModule } from 'primeng/toast';
 
 //Componenets
 import { AppRoutingModule } from './app-routing.module';
